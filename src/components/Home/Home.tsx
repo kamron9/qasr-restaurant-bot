c81f1c8509d29457d3fe9b149ca0cf95d444fc9b
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-05-02 16:49:42 +0500

    test

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { DrawerContext } from '../../context/DrawerContext'
 import { ProductContext } from '../../context/ProductProvider'
 import BasketDrawer from '../Drawer'
@@ -6,6 +6,13 @@ import Tabs from '../Tabs'
 import styles from './home.module.css'
 
 const Home = () => {
+	const tg = window.Telegram.WebApp
+
+	useEffect(() => {
+		tg.ready()
+	}, [])
+	console.log(tg?.initDataUnsafe)
+
 	const { productState } = useContext(ProductContext)
 	const { toggleDrawer } = useContext(DrawerContext)
 	return (
